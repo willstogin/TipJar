2f5fc50ac302e2c43e208cd9c5826c4ead5bf8a9
Sasha Weiss <sasha.weiss@u.northwestern.edu>
2014-11-22 21:23:44 -0600

    Test #1

---
+++ app/src/main/java/sashawilljerry/tipjar/Login.java
@@ -0,0 +1,66 @@
+package sashawilljerry.tipjar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
+
+
+public class Login extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view)
+    {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "in-v3.mailjet.com");
+        Session session = Session.getInstance(props, null);
+
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            msg.setFrom("sasha.weiss@u.northwestern.edu");
+            msg.setRecipients(Message.RecipientType.TO, "jpsun2@illinois.edu");
+            msg.setSubject("JavaMail hello world example");
+            msg.setSentDate(new Date());
+            msg.setText("Hello, world!\n");
+            Transport.send(msg, "79b28e261e95381c54fba0078672d434", "94cc9e45a80b3f07fa0a10ded9fea14d");
+        } catch (MessagingException mex) {
+            System.out.println("send failed, exception: " + mex);
+        }
+
+    }
+
+}
