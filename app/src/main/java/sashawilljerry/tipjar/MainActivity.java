47ef43df571fad098e257edd4d135b4d65f258ed
Sasha Weiss <sasha.weiss@u.northwestern.edu>
2014-11-22 19:34:39 -0600

    Test #1

--- app/src/main/java/sashawilljerry/tipjar/MainActivity.java
+++ app/src/main/java/sashawilljerry/tipjar/MainActivity.java
@@ -1,11 +1,9 @@
 package sashawilljerry.tipjar;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -39,10 +37,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**Called when the user clicks the Send button
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-    }
-     */
 }
